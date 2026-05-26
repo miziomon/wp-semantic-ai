c48fbd99de42298ad9ea94c09e1334ab89348889
Maurizio <maurizio@mavida.com>
2026-05-26 11:23:55 +0200

    chore: add build/index.asset.php stub for PHPStan resolution

---
+++ build/index.asset.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * File asset stub per PHPStan e sviluppo locale.
+ * Viene sovrascritto da "npm run build" con deps reali e version hash.
+ *
+ * @return array{dependencies: string[], version: string}
+ */
+return [
+	'dependencies' => [],
+	'version'      => '0.0.0-dev',
+];
